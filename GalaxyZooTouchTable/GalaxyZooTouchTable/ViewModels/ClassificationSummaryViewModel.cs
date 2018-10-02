c3c8bd33388a25a51166065b7392af3699ce3cfe
Will Granger <twgranger@gmail.com>
2018-10-02 14:39:53 -0500

    Create SubjectViewer View

---
+++ GalaxyZooTouchTable/GalaxyZooTouchTable/ViewModels/ClassificationSummaryViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel;
+
+namespace GalaxyZooTouchTable.ViewModels
+{
+    public class ClassificationSummaryViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnPropertyRaised(string propertyname)
+        {   if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyname));
+        }
+    }
+}
