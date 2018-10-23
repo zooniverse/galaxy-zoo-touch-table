c52c11bcec2f3159e6b1b811df0a2cb26fb45e43
Will Granger <twgranger@gmail.com>
2018-10-23 10:16:40 -0500

    Clean Up Opacity Animations

---
+++ GalaxyZooTouchTable/GalaxyZooTouchTable/Converters/ObjectEqualityStringConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace GalaxyZooTouchTable.Converters
+{
+    public class ObjectEqualityStringConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value != null)
+            {
+                return value.GetType().Name == parameter as string;
+            }
+            return true;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
