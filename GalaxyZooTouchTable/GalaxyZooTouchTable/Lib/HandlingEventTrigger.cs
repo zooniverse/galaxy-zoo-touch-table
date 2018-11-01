074944965cf1bbfe33758eefd2eaf1fb250787a4
Will Granger <twgranger@gmail.com>
2018-11-01 13:27:12 -0500

    Enlarge Hit Area for Galaxy Examples

---
+++ GalaxyZooTouchTable/GalaxyZooTouchTable/Lib/HandlingEventTrigger.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Windows;
+
+namespace GalaxyZooTouchTable.Lib
+{
+    public class HandlingEventTrigger : System.Windows.Interactivity.EventTrigger
+    {
+        protected override void OnEvent(EventArgs eventArgs)
+        {
+            var routedEventArgs = eventArgs as RoutedEventArgs;
+            if (routedEventArgs != null)
+                routedEventArgs.Handled = true;
+
+            base.OnEvent(eventArgs);
+        }
+    }
+}
