d916990306116ada3bee540c3fba0ddff7295f08
Will Granger <twgranger@gmail.com>
2018-11-16 14:35:34 -0600

    Adjust Image Size and DataContext Finder

--- GalaxyZooTouchTable/GalaxyZooTouchTable/Views/MainWindow.xaml.cs
+++ GalaxyZooTouchTable/GalaxyZooTouchTable/Views/MainWindow.xaml.cs
@@ -12,7 +12,6 @@ namespace GalaxyZooTouchTable
         public MainWindow()
         {
             InitializeComponent();
-            DataContext = new MainWindowViewModel();
             this.Cursor = Cursors.None;
         }
     }
