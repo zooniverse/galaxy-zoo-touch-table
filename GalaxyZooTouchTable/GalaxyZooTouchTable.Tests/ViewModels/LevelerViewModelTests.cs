65b64ec1dac5d5ebf36904c0fae2fd60a0a94294
Will Granger <twgranger@gmail.com>
2019-01-17 14:27:20 -0600

    Set Up Tests with LevelerViewModel

---
+++ GalaxyZooTouchTable/GalaxyZooTouchTable.Tests/ViewModels/LevelerViewModelTests.cs
@@ -0,0 +1,19 @@
+﻿using GalaxyZooTouchTable.Models;
+using GalaxyZooTouchTable.ViewModels;
+using Xunit;
+
+namespace GalaxyZooTouchTable.Tests.ViewModels
+{
+    public class LevelerViewModelTests
+    {
+        [Fact]
+        public void ShouldIncrementCount()
+        {
+            var starUser = new StarUser();
+            var viewModel = new LevelerViewModel(starUser);
+
+            viewModel.OnIncrementCount(5);
+            Assert.Equal(5, viewModel.ClassificationsThisSession);
+        }
+    }
+}
