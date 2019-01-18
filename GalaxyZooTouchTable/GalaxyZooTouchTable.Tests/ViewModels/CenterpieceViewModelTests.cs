5f7cc5a0d35a291299120664a70659b522addd89
Will Granger <twgranger@gmail.com>
2019-01-18 13:36:36 -0600

    Write CenterpieceViewModelTests

---
+++ GalaxyZooTouchTable/GalaxyZooTouchTable.Tests/ViewModels/CenterpieceViewModelTests.cs
@@ -0,0 +1,34 @@
+﻿using GalaxyZooTouchTable.Lib;
+using GalaxyZooTouchTable.ViewModels;
+using System;
+using Xunit;
+
+namespace GalaxyZooTouchTable.Tests.ViewModels
+{
+    public class CenterpieceViewModelTests
+    {
+        private CenterpieceViewModel ViewModel = new CenterpieceViewModel();
+
+        [Fact]
+        private void ShouldInstantiateWithDefaultValues()
+        {
+            Assert.True(ViewModel.AllUsers.Count > 0);
+            Assert.True(ViewModel.ShowJoinMessage);
+            Assert.False(ViewModel.CenterpieceIsFlipped);
+        }
+
+        [Fact]
+        private void ShouldHideJoinMessageWithActiveUser()
+        {
+            GlobalData.GetInstance().StarUser.Active = true;
+            Assert.False(ViewModel.ShowJoinMessage);
+        }
+
+        [Fact]
+        private void ShouldFlipCenterpiece()
+        {
+            ViewModel.OnFlipCenterpiece(null, new EventArgs());
+            Assert.True(ViewModel.CenterpieceIsFlipped);
+        }
+    }
+}
