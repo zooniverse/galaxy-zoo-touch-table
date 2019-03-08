4c46c98232608d401373866837d602a8ccbab1a6
Will Granger <twgranger@gmail.com>
2019-03-08 15:10:33 -0600

    Refactor LocalDBService

--- GalaxyZooTouchTable/GalaxyZooTouchTable/Services/LocalDBService.cs
+++ GalaxyZooTouchTable/GalaxyZooTouchTable/Services/LocalDBService.cs
@@ -6,30 +6,10 @@ namespace GalaxyZooTouchTable.Services
 {
     public static class LocalDBService
     {
-        public static List<TableSubject> GetLocalSubjects(double minRa = 0, double maxRa = 360, double minDec = -90, double maxDec = 90)
-        {
-            using (SQLiteConnection connection = new SQLiteConnection("Data Source=C:\\sqlite\\databases\\test_database.db"))
-            {
-                connection.Open();
-                string query = $"select * from Subjects where dec > {minDec} and dec < {maxDec} and ra > {minRa} and ra < {maxRa}";
-                SQLiteCommand command = new SQLiteCommand(query, connection);
-                SQLiteDataReader reader = command.ExecuteReader();
-                List<TableSubject> SubjectsWithinBounds = new List<TableSubject>();
-
-                while (reader.Read())
-                {
-                    string id = reader["subject_id"] as string;
-                    string image = reader["image"] as string;
-                    double ra = (double)reader["ra"];
-                    double dec = (double)reader["dec"];
-                    TableSubject subjectFromDatabase = new TableSubject(id, image, ra, dec);
-                    SubjectsWithinBounds.Add(subjectFromDatabase);
-                }
-
-                connection.Close();
-                return SubjectsWithinBounds;
-            }
-        }
+        private static string HighestRaQuery = "select * from Subjects order by ra desc limit 1";
+        private static string HighestDecQuery = "select * from Subjects order by dec desc limit 1";
+        private static string LowestRaQuery = "select * from Subjects order by ra asc limit 1";
+        private static string LowestDecQuery = "select * from Subjects order by dec asc limit 1";
 
         public static TableSubject GetLocalSubject(string id)
         {
@@ -56,10 +36,21 @@ namespace GalaxyZooTouchTable.Services
 
         public static List<TableSubject> GetQueuedSubjects()
         {
+            string query = "select * from Subjects order by classifications_count asc limit 10";
+            return GetSubjects(query);
+        }
+
+        public static List<TableSubject> GetLocalSubjects(double minRa = 0, double maxRa = 360, double minDec = -90, double maxDec = 90)
+        {
+            string query = $"select * from Subjects where dec > {minDec} and dec < {maxDec} and ra > {minRa} and ra < {maxRa}";
+            return GetSubjects(query);
+        }
+
+        public static List<TableSubject> GetSubjects(string query)
+        {
             using (SQLiteConnection connection = new SQLiteConnection("Data Source=C:\\sqlite\\databases\\test_database.db"))
             {
                 connection.Open();
-                string query = $"select * from Subjects order by classifications_count asc limit 10";
                 SQLiteCommand command = new SQLiteCommand(query, connection);
                 SQLiteDataReader reader = command.ExecuteReader();
                 List<TableSubject> Subjects = new List<TableSubject>();
@@ -79,12 +70,11 @@ namespace GalaxyZooTouchTable.Services
             }
         }
 
-        public static SpacePoint GetRandomPoint()
+        public static SpacePoint GetPoint(string query)
         {
             using (SQLiteConnection connection = new SQLiteConnection("Data Source=C:\\sqlite\\databases\\test_database.db"))
             {
                 connection.Open();
-                string query = $"select * from Subjects order by random() limit 1";
                 SQLiteCommand command = new SQLiteCommand(query, connection);
                 SQLiteDataReader reader = command.ExecuteReader();
                 SpacePoint point = null;
@@ -99,94 +89,64 @@ namespace GalaxyZooTouchTable.Services
                 connection.Close();
                 return point;
             }
+        }
+
+        public static SpacePoint GetRandomPoint()
+        {
+            string query = "select * from Subjects order by random() limit 1";
+            return GetPoint(query);
         }
 
         public static SpacePoint FindNextAscendingRa(double RaLowerBounds)
         {
-            using (SQLiteConnection connection = new SQLiteConnection("Data Source=C:\\sqlite\\databases\\test_database.db"))
+            string query = $"select * from Subjects where ra > {RaLowerBounds} limit 1";
+            SpacePoint Center = GetPoint(query);
+
+            if (Center == null)
             {
-                connection.Open();
-                string query = $"select * from Subjects where ra > {RaLowerBounds} limit 1";
-                SQLiteCommand command = new SQLiteCommand(query, connection);
-                SQLiteDataReader reader = command.ExecuteReader();
-                SpacePoint point = null;
-
-                while (reader.Read())
-                {
-                    double ra = (double)reader["ra"];
-                    double dec = (double)reader["dec"];
-                    point = new SpacePoint(ra, dec);
-                }
-
-                connection.Close();
-                return point;
+                Center = GetPoint(LowestRaQuery);
             }
+
+            return Center;
         }
 
         public static SpacePoint FindNextDescendingRa(double RaUpperBounds)
         {
-            using (SQLiteConnection connection = new SQLiteConnection("Data Source=C:\\sqlite\\databases\\test_database.db"))
+            string query = $"select * from Subjects where ra < {RaUpperBounds} limit 1";
+            SpacePoint Center = GetPoint(query);
+
+            if (Center == null)
             {
-                connection.Open();
-                string query = $"select * from Subjects where ra < {RaUpperBounds} limit 1";
-                SQLiteCommand command = new SQLiteCommand(query, connection);
-                SQLiteDataReader reader = command.ExecuteReader();
-                SpacePoint point = null;
-
-                while (reader.Read())
-                {
-                    double ra = (double)reader["ra"];
-                    double dec = (double)reader["dec"];
-                    point = new SpacePoint(ra, dec);
-                }
-
-                connection.Close();
-                return point;
+                Center = GetPoint(HighestRaQuery);
             }
+
+            return Center;
         }
 
         public static SpacePoint FindNextAscendingDec(double DecLowerBounds)
         {
-            using (SQLiteConnection connection = new SQLiteConnection("Data Source=C:\\sqlite\\databases\\test_database.db"))
+            string query = $"select * from Subjects where dec > {DecLowerBounds} limit 1";
+            SpacePoint Center = GetPoint(query);
+
+            if (Center == null)
             {
-                connection.Open();
-                string query = $"select * from Subjects where dec > {DecLowerBounds} limit 1";
-                SQLiteCommand command = new SQLiteCommand(query, connection);
-                SQLiteDataReader reader = command.ExecuteReader();
-                SpacePoint point = null;
-
-                while (reader.Read())
-                {
-                    double dec = (double)reader["dec"];
-                    double ra = (double)reader["ra"];
-                    point = new SpacePoint(ra, dec);
-                }
-
-                connection.Close();
-                return point;
+                Center = GetPoint(LowestDecQuery);
             }
+
+            return Center;
         }
 
         public static SpacePoint FindNextDescendingDec(double DecUpperBounds)
         {
-            using (SQLiteConnection connection = new SQLiteConnection("Data Source=C:\\sqlite\\databases\\test_database.db"))
+            string query = $"select * from Subjects where dec < {DecUpperBounds} limit 1";
+            SpacePoint Center = GetPoint(query);
+
+            if (Center == null)
             {
-                connection.Open();
-                string query = $"select * from Subjects where dec < {DecUpperBounds} limit 1";
-                SQLiteCommand command = new SQLiteCommand(query, connection);
-                SQLiteDataReader reader = command.ExecuteReader();
-                SpacePoint point = null;
-
-                while (reader.Read())
-                {
-                    double dec = (double)reader["dec"];
-                    double ra = (double)reader["ra"];
-                    point = new SpacePoint(ra, dec);
-                }
-
-                connection.Close();
-                return point;
+                Center = GetPoint(HighestDecQuery);
             }
+
+            return Center;
         }
     }
 }
