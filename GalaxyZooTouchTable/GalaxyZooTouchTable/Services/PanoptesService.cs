428c4653fdc2eb1e08544bd1e7c85d0550e154d9
Will Granger <twgranger@gmail.com>
2019-04-10 12:16:42 -0500

    Set up a queue to process failed classifications

--- GalaxyZooTouchTable/GalaxyZooTouchTable/Services/PanoptesService.cs
+++ GalaxyZooTouchTable/GalaxyZooTouchTable/Services/PanoptesService.cs
@@ -1,7 +1,9 @@
 ﻿using PanoptesNetClient;
 using PanoptesNetClient.Models;
+using System;
 using System.Collections.Generic;
 using System.Collections.Specialized;
+using System.Net.Http;
 using System.Threading.Tasks;
 
 namespace GalaxyZooTouchTable.Services
@@ -9,10 +11,31 @@ namespace GalaxyZooTouchTable.Services
     public class PanoptesService : IPanoptesService
     {
         ApiClient _panoptesClient = new ApiClient();
+        List<Classification> QueuedClassifications { get; set; } = new List<Classification>();
 
         public async Task CreateClassificationAsync(Classification classification)
         {
-            await _panoptesClient.Classifications.Create(classification);
+            QueuedClassifications.Add(classification);
+            await SaveAllQueuedClassifications();
+        }
+
+        private async Task SaveAllQueuedClassifications()
+        {
+            List<Classification> newQueue = new List<Classification>();
+            foreach (Classification classification in QueuedClassifications)
+            {
+                try
+                {
+                    HttpResponseMessage response = await _panoptesClient.Classifications.Create(classification);
+                    if ((int)response.StatusCode != 422) response.EnsureSuccessStatusCode();
+                }
+                catch (HttpRequestException error)
+                {
+                    newQueue.Add(classification);
+                    System.Console.WriteLine(error.Message);
+                }
+            }
+            QueuedClassifications = newQueue;
         }
 
         public async Task<Subject> GetSubjectAsync(string id)
