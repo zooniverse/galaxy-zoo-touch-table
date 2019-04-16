eddb78ee149384841e3b8aba54d1296166d21132
Will Granger <twgranger@gmail.com>
2019-04-16 15:13:29 -0500

    Tighten Up Animations and Fades

--- GalaxyZooTouchTable/GalaxyZooTouchTable/Views/SpaceView.xaml.cs
+++ GalaxyZooTouchTable/GalaxyZooTouchTable/Views/SpaceView.xaml.cs
@@ -28,20 +28,20 @@ namespace GalaxyZooTouchTable.Views
 
             switch (direction) {
                 case "North":
-                    startingPoint = new Point(0, (int)CurrentCutout.Height);
-                    endingPoint = new Point(0, (int)CurrentCutout.Height * -1);
-                    break;
-                case "South":
                     startingPoint = new Point(0, (int)CurrentCutout.Height * -1);
                     endingPoint = new Point(0, (int)CurrentCutout.Height);
                     break;
-                case "East":
-                    startingPoint = new Point((int)CurrentCutout.Width * -1, 0);
-                    endingPoint = new Point((int)CurrentCutout.Width, 0);
+                case "South":
+                    startingPoint = new Point(0, (int)CurrentCutout.Height);
+                    endingPoint = new Point(0, (int)CurrentCutout.Height * -1);
                     break;
-                case "West":
+                case "East":
                     startingPoint = new Point((int)CurrentCutout.Width, 0);
                     endingPoint = new Point((int)CurrentCutout.Width * -1, 0);
+                    break;
+                case "West":
+                    startingPoint = new Point((int)CurrentCutout.Width * -1, 0);
+                    endingPoint = new Point((int)CurrentCutout.Width, 0);
                     break;
                 default:
                     Console.WriteLine("Unknown Direction");
@@ -50,22 +50,22 @@ namespace GalaxyZooTouchTable.Views
             TransformGroup previousCutoutImageGroup = new TransformGroup();
             TransformGroup previousCutoutBorderGroup = new TransformGroup();
             ScaleTransform scaleDown = new ScaleTransform();
-            DoubleAnimation animateScaleDown = new DoubleAnimation(1, 0.5, TimeSpan.FromSeconds(1));
-            animateScaleDown.BeginTime = TimeSpan.FromSeconds(0.5);
+            DoubleAnimation animateScaleDown = new DoubleAnimation(1, 0.5, TimeSpan.FromSeconds(0.25));
+            animateScaleDown.BeginTime = TimeSpan.FromSeconds(0.25);
             scaleDown.BeginAnimation(ScaleTransform.ScaleXProperty, animateScaleDown);
             scaleDown.BeginAnimation(ScaleTransform.ScaleYProperty, animateScaleDown);
 
             ScaleTransform scaleUp = new ScaleTransform();
-            DoubleAnimation animateScaleUp = new DoubleAnimation(1, 2, TimeSpan.FromSeconds(1));
-            animateScaleUp.BeginTime = TimeSpan.FromSeconds(2.5);
+            DoubleAnimation animateScaleUp = new DoubleAnimation(1, 2, TimeSpan.FromSeconds(0.25));
+            animateScaleUp.BeginTime = TimeSpan.FromSeconds(1);
             scaleUp.BeginAnimation(ScaleTransform.ScaleXProperty, animateScaleUp);
             scaleUp.BeginAnimation(ScaleTransform.ScaleYProperty, animateScaleUp);
 
             TranslateTransform moveAside = new TranslateTransform();
-            DoubleAnimation animateToX = new DoubleAnimation(endingPoint.X, TimeSpan.FromSeconds(1));
-            DoubleAnimation animateToY = new DoubleAnimation(endingPoint.Y, TimeSpan.FromSeconds(1));
-            animateToX.BeginTime = TimeSpan.FromSeconds(1.5);
-            animateToY.BeginTime = TimeSpan.FromSeconds(1.5);
+            DoubleAnimation animateToX = new DoubleAnimation(endingPoint.X, TimeSpan.FromSeconds(0.25));
+            DoubleAnimation animateToY = new DoubleAnimation(endingPoint.Y, TimeSpan.FromSeconds(0.25));
+            animateToX.BeginTime = TimeSpan.FromSeconds(0.5);
+            animateToY.BeginTime = TimeSpan.FromSeconds(0.5);
             moveAside.BeginAnimation(TranslateTransform.XProperty, animateToX);
             moveAside.BeginAnimation(TranslateTransform.YProperty, animateToY);
 
@@ -80,10 +80,10 @@ namespace GalaxyZooTouchTable.Views
             TransformGroup currentCutoutImageGroup = new TransformGroup();
             TranslateTransform startAside = new TranslateTransform(startingPoint.X, startingPoint.Y);
             TranslateTransform moveInto = new TranslateTransform();
-            DoubleAnimation animateIntoX = new DoubleAnimation(endingPoint.X, TimeSpan.FromSeconds(1));
-            DoubleAnimation animateIntoY = new DoubleAnimation(endingPoint.Y, TimeSpan.FromSeconds(1));
-            animateIntoX.BeginTime = TimeSpan.FromSeconds(1.5);
-            animateIntoY.BeginTime = TimeSpan.FromSeconds(1.5);
+            DoubleAnimation animateIntoX = new DoubleAnimation(endingPoint.X, TimeSpan.FromSeconds(0.25));
+            DoubleAnimation animateIntoY = new DoubleAnimation(endingPoint.Y, TimeSpan.FromSeconds(0.25));
+            animateIntoX.BeginTime = TimeSpan.FromSeconds(0.75);
+            animateIntoY.BeginTime = TimeSpan.FromSeconds(0.75);
             moveInto.BeginAnimation(TranslateTransform.XProperty, animateIntoX);
             moveInto.BeginAnimation(TranslateTransform.YProperty, animateIntoY);
 
@@ -91,17 +91,31 @@ namespace GalaxyZooTouchTable.Views
             currentCutoutBorderGroup.Children.Add(moveInto);
             currentCutoutImageGroup.Children.Add(scaleDown);
             currentCutoutImageGroup.Children.Add(scaleUp);
-            CurrentCutoutImage.RenderTransform = currentCutoutBorderGroup;
-            CurrentCutout.RenderTransform = currentCutoutImageGroup;
+            CurrentCutoutImage.RenderTransform = currentCutoutImageGroup;
+            CurrentCutout.RenderTransform = currentCutoutBorderGroup;
 
             FadeOutGalaxies();
         }
 
         private void FadeOutGalaxies()
         {
-            DoubleAnimation anim = new DoubleAnimation(0, TimeSpan.FromSeconds(0.5));
-            anim.AutoReverse = true;
-            VisibleGalaxies.BeginAnimation(OpacityProperty, anim);
+            DoubleAnimation animateOut = new DoubleAnimation(1, 0, TimeSpan.FromSeconds(0));
+            DoubleAnimation animateIn = new DoubleAnimation(0, 1, TimeSpan.FromSeconds(0.5));
+            animateIn.BeginTime = TimeSpan.FromSeconds(1.5);
+
+            Storyboard storyboard = new Storyboard();
+            Storyboard.SetTargetName(animateOut, VisibleGalaxies.Name);
+            Storyboard.SetTargetName(animateIn, VisibleGalaxies.Name);
+
+            Storyboard.SetTargetProperty(animateOut,
+                new System.Windows.PropertyPath(OpacityProperty));
+            Storyboard.SetTargetProperty(animateIn,
+                new System.Windows.PropertyPath(OpacityProperty));
+
+            storyboard.Children.Add(animateOut);
+            storyboard.Children.Add(animateIn);
+
+            storyboard.Begin(this);
         }
     }
 }
