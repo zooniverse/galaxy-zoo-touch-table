6be1c89fe2a8b1ce82e4560c4c796b2973332edb
Will Granger <twgranger@gmail.com>
2019-05-15 10:02:37 -0500

    Add New Example Images

--- GalaxyZooTouchTable/GalaxyZooTouchTable/Models/GalaxyExample.cs
+++ GalaxyZooTouchTable/GalaxyZooTouchTable/Models/GalaxyExample.cs
@@ -15,6 +15,9 @@ namespace GalaxyZooTouchTable.Models
         BitmapImage ImageOne { get; }
         BitmapImage ImageTwo { get; set; }
         BitmapImage ImageThree { get; set; }
+        BitmapImage ImageFour { get; set; }
+        BitmapImage ImageFive { get; set; }
+        BitmapImage ImageSix { get; set; }
         string Title { get; set; }
         string Description { get; set; }
         string Description2 { get; set; }
@@ -42,6 +45,9 @@ namespace GalaxyZooTouchTable.Models
             public BitmapImage ImageOne => new BitmapImage(new Uri("pack://application:,,,/Images/GalaxyExamples/smooth1.jpg"));
             public BitmapImage ImageTwo => new BitmapImage(new Uri("pack://application:,,,/Images/GalaxyExamples/smooth2.jpg"));
             public BitmapImage ImageThree => new BitmapImage(new Uri("pack://application:,,,/Images/GalaxyExamples/smooth3.jpg"));
+            public BitmapImage ImageFour => new BitmapImage(new Uri("pack://application:,,,/Images/GalaxyExamples/smooth4.jpg"));
+            public BitmapImage ImageFive => new BitmapImage(new Uri("pack://application:,,,/Images/GalaxyExamples/smooth5.jpg"));
+            public BitmapImage ImageSix => new BitmapImage(new Uri("pack://application:,,,/Images/GalaxyExamples/smooth6.jpg"));
             public string Title => "Smooth";
             public string Description => "Smooth galaxies fade gradually in all directions from the center.";
             public string Description2 => "There may be a small bright symmetric core.";
@@ -52,6 +58,9 @@ namespace GalaxyZooTouchTable.Models
             public BitmapImage ImageOne => new BitmapImage(new Uri("pack://application:,,,/Images/GalaxyExamples/features1.jpeg"));
             public BitmapImage ImageTwo => new BitmapImage(new Uri("pack://application:,,,/Images/GalaxyExamples/features2.jpg"));
             public BitmapImage ImageThree => new BitmapImage(new Uri("pack://application:,,,/Images/GalaxyExamples/features3.jpg"));
+            public BitmapImage ImageFour => new BitmapImage(new Uri("pack://application:,,,/Images/GalaxyExamples/features4.jpg"));
+            public BitmapImage ImageFive => new BitmapImage(new Uri("pack://application:,,,/Images/GalaxyExamples/features5.jpg"));
+            public BitmapImage ImageSix => new BitmapImage(new Uri("pack://application:,,,/Images/GalaxyExamples/features6.jpg"));
             public string Title => "Features";
             public string Description => "Galaxies might have spiral arms, a bulge or bar, or any other interesting feature.";
             public string Description2 => "Choose this option if you see anything unique about the galaxy.";
@@ -62,6 +71,9 @@ namespace GalaxyZooTouchTable.Models
             public BitmapImage ImageOne => new BitmapImage(new Uri("pack://application:,,,/Images/GalaxyExamples/star1.jpg"));
             public BitmapImage ImageTwo => new BitmapImage(new Uri("pack://application:,,,/Images/GalaxyExamples/star2.jpg"));
             public BitmapImage ImageThree => new BitmapImage(new Uri("pack://application:,,,/Images/GalaxyExamples/star3.jpg"));
+            public BitmapImage ImageFour => new BitmapImage(new Uri("pack://application:,,,/Images/GalaxyExamples/star4.jpg"));
+            public BitmapImage ImageFive => new BitmapImage(new Uri("pack://application:,,,/Images/GalaxyExamples/star5.jpg"));
+            public BitmapImage ImageSix => new BitmapImage(new Uri("pack://application:,,,/Images/GalaxyExamples/star6.jpg"));
             public string Title => "Not a Galaxy";
             public string Description => "Choose \"Not a Galaxy\" if there is no central galaxy to classify or if the artifact is so badly displayed that you can't ignore it and classify the galaxy with reasonable confidence.";
             public string Description2 => "The telescopes taking our data were designed to look at faint galaxies, which means that bright, compact objects like stars sometimes look a bit strange.";
