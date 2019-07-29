9788436af45b409e317fc0fda5e26a739f09f976
Will Granger <twgranger@gmail.com>
2019-07-29 08:59:59 -0500

    Return if Request is Null

--- GalaxyZooTouchTable/GalaxyZooTouchTable/ViewModels/NotificationsViewModel.cs
+++ GalaxyZooTouchTable/GalaxyZooTouchTable/ViewModels/NotificationsViewModel.cs
@@ -334,8 +334,9 @@ namespace GalaxyZooTouchTable.ViewModels
         {
             PendingRequest Request = PendingRequests.FirstOrDefault();
             HelpNotification Notification = new HelpNotification(User, HelpNotificationStatus.Decline);
-            Messenger.Default.Send(Notification, $"{Request.CooperatingPeer.Name}_PostNotification");
             ResetNotifications();
+            if (Request == null) return;
+            Messenger.Default.Send(Notification, $"{Request.CooperatingPeer.Name}_PostNotification");
             LogEvent(entry: "Decline_Galaxy", peer: Request.CooperatingPeer.Name);
         }
 
